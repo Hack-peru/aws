4090511daf5612374bbfc2a905d9bbaec7cc5a1c
Hack-peru <97358615+Hack-peru@users.noreply.github.com>
2023-10-18 11:24:41 -0500

    Update websocketsshonly.sh

--- websocketsshonly.sh
+++ websocketsshonly.sh
@@ -3,14 +3,14 @@
 
 cd 
 # Installing Service ws edu-ovpn
-wget -q -O /usr/local/bin/edu-ovpn "https://raw.githubusercontent.com/syapik96/aws/main/lain2/ovpn.py"
+wget -q -O /usr/local/bin/edu-ovpn "https://raw.githubusercontent.com/Hack-peru/aws/main/lain2/ovpn.py"
 chmod +x /usr/local/bin/edu-ovpn
 
 # Create system Service ws edu-ovpn
 cat > /etc/systemd/system/edu-ovpn.service <<END
 [Unit]
 Description=Edu-Ovpn Python Over Websocket 
-Documentation=https://github.com/syapik96/aws
+Documentation=https://github.com/Hack-peru/aws
 After=network.target nss-lookup.target
 
 [Service]
@@ -27,14 +27,14 @@ WantedBy=multi-user.target
 END
 
 # Installing Service ws-dropbear
-wget -q -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/syapik96/aws/main/lain2/dropbear-ws.py"
+wget -q -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/Hack-peru/aws/main/lain2/dropbear-ws.py"
 chmod +x /usr/local/bin/ws-dropbear
 
 # Create system Service ws-dropbear
 cat > /etc/systemd/system/ws-dropbear.service <<END
 [Unit]
 Description=Dropbear Python Over Websocket
-Documentation=https://github.com/syapik96/aws
+Documentation=https://github.com/Hack-peru/aws
 After=network.target nss-lookup.target
 
 [Service]
@@ -51,7 +51,7 @@ WantedBy=multi-user.target
 END
 
 # Installing Service ws-stunnel
-wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/syapik96/aws/main/lain2/ws-stunnel.py"
+wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/Hack-peru/aws/main/lain2/ws-stunnel.py"
 chmod +x /usr/local/bin/ws-stunnel
 
 # Create system Service ws-stunnel
@@ -75,14 +75,14 @@ WantedBy=multi-user.target
 END
 
 # Installing Service edu-openssh
-wget -O /usr/local/bin/edu-openssh "https://raw.githubusercontent.com/syapik96/aws/main/lain2/edu-openssh.py"
+wget -O /usr/local/bin/edu-openssh "https://raw.githubusercontent.com/Hack-peru/aws/main/lain2/edu-openssh.py"
 chmod +x /usr/local/bin/edu-openssh
 
 # Create system Service edu-openssh
 cat > /etc/systemd/system/edu-openssh.service <<END
 [Unit]
 Description=Python Openssh Proxy Websocket
-Documentation=https://github.com/syapik96/aws
+Documentation=https://github.com/Hack-peru/aws
 After=network.target nss-lookup.target
 
 [Service]
